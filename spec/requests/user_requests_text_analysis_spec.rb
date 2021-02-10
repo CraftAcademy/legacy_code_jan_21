08bd15a49bb210e830d362ecf5d9ffac8f9da676
FoxyTony <mirantony@icloud.com>
2021-02-10 14:58:30 +0100

    Added some params to test request

--- spec/requests/user_requests_text_analysis_spec.rb
+++ spec/requests/user_requests_text_analysis_spec.rb
@@ -1,10 +1,15 @@
 RSpec.describe 'POST /api/analyses', types: :request do
   describe 'successfully' do
     before do
-      post '/api/analyses'
+      post '/api/analyses', params: {
+        analysis: {
+          category: "text",
+          resource: "Hello there Lucas"
+        }
+      }
     end
 
-    it 'responds with a 201 stats' do
+    it 'responds with a 201 status' do
       expect(response).to have_http_status 201
     end
 
