383d2700d7a3116a28536fe1a01ebd7d8d9db32e
Lucas Knudsen <laksteelhouse@gmail.com>
2021-02-12 10:59:13 +0100

    Good request returns 201 status and a hash with out initial resource

--- spec/requests/user_can_request_image_analysis_spec.rb
+++ spec/requests/user_can_request_image_analysis_spec.rb
@@ -4,21 +4,21 @@ RSpec.describe 'POST /api/analyses', types: :request do
       post '/api/analyses', params: {
         analysis: {
           category: 'image',
-          resource: 'https://metro.co.uk/wp-content/uploads/2014/11/1400x931390-e1416921236602.jpg'
+          resource: 'https://pbs.twimg.com/media/B9ahb_-IcAEqVFL.jpg'
         }
       }
     end
 
     it 'responds with a 201 status' do
-      binding.pry
       expect(response).to have_http_status 201
     end
 
     it 'returns a results hash with our initial resource' do
-      
-      expected_output = 'I just fucking love testing'
-      expect(response_json['results']['text']).to eq expected_output
+      expected_output = 'https://pbs.twimg.com/media/B9ahb_-IcAEqVFL.jpg'
+      expect(response_json['resource']).to eq expected_output
     end
+
+    
 
   end
 end
